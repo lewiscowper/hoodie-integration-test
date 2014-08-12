aaa15814e79129ada8834e5ce4c28287c96aed00
Gregor Martynus <gregor@martynus.net>
2014-08-12 16:49:42 +0200

    [wip] currently failing signup / signout / signin test

--- tests/dalek/signup-signout-signin.js
+++ tests/dalek/signup-signout-signin.js
@@ -8,24 +8,29 @@ module.exports = {
   'open app, sign up, sign out, sign in': function(test) {
     test
       .open(hosts.www)
-      .assert.visible('[data-hoodie-action=signup]')
+      .assert.visible('[data-hoodie-action=signup]', 'Sign up button visible')
       .click('[data-hoodie-action=signup]')
-      .assert.visible('[name=password_confirmation]')
+      .assert.visible('.modal form', 'Sign up form visible')
       .type('[name=username]', username)
       .type('[name=password]', password)
       .type('[name=password_confirmation]', password)
       .submit('.modal form')
-      .assert.visible('.hoodie-username')
-      .assert.text('.hoodie-username').to.contain('dalekuser')
+      .waitFor(function() {
+        return !!hoodie.account.username
+      })
+      .assert.visible('.hoodie-account-signedin', 'Sign out button visible')
+      .assert.text('.hoodie-account-signedin .hoodie-username').to.contain('dalekuser')
       .click('[data-hoodie-action=signout]')
-      .assert.visible('[data-hoodie-action=signup]')
       .click('.hoodie-account-signedout [data-toggle=dropdown]')
       .click('[data-hoodie-action=signin]')
-      .assert.visible('[name=username]')
+      .assert.visible('.modal form', 'Sign in form visible')
       .type('[name=username]', username)
       .type('[name=password]', password)
       .submit('.modal form')
-      .assert.visible('.hoodie-username')
-      .assert.text('.hoodie-username').to.contain('dalekuser');
+      .waitFor(function() {
+        return !hoodie.account.username
+      })
+      .assert.visible('.hoodie-account-signedin .hoodie-username')
+      .assert.text('.hoodie-account-signedin .hoodie-username').to.contain('dalekuser');
   },
 };
